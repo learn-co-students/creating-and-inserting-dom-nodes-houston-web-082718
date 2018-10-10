d107128842dfa98aee437e35e3f14ad32ed9cd72
V10LET <violet.wmoon@gmail.com>
2018-10-10 17:01:38 -0500

    creating and deleting shit from the DOM

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+//----> Creates h1 with styling and adds to DOM
+const title = document.createElement('h1')
+title.innerHTML = 'Violet Moon\'s Website'
+title.style.fontStyle = 'oblique'
+title.style.textAlign = 'center'
+document.body.appendChild(title)
+
+//----> Creates list and adds to DOM
+const animalsList = document.createElement('ul')
+for (let i = 1; i < 5; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = i +' giraffey'
+  animalsList.appendChild(li)
+}
+document.body.appendChild(animalsList)
+
+//----> Removes list item from <ul></ul>
+animalsList.removeChild(animalsList.querySelector('li:nth-child(3)'))
+
+//----> Removes list
+animalsList.remove()
